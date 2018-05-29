8a60f76a56a48f122d7d2d05f61f56ae488bcbdc
leonyan <leonyan.loveoflife@gmail.com>
2018-05-29 14:25:10 +0800

    create the normal user table without restricts

--- MoreResources/users/models.py
+++ MoreResources/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class normal_user(models.Model):
+	user_id = models.IntField()
+	user_name = models.CharField(max_length = 64)
+	user_passwd = models.CharField(max_length = 32)
+	point = models.IntField()
+	type = models.CharField(max_length = 16)
+	image = models.BlobField()
+	introduction = models.CharField(max_length = 100)

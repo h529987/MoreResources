4e80aff38ea8d26cd057df87c480244e45ce922d
leonyan <leonyan.loveoflife@gmail.com>
2018-05-29 14:31:49 +0800

    make user_id becoming the primary key of normal_user tabel

--- MoreResources/users/models.py
+++ MoreResources/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class normal_user(models.Model):
-	user_id = models.IntField()
+	user_id = models.IntField(primary_key = True)
 	user_name = models.CharField(max_length = 64)
 	user_passwd = models.CharField(max_length = 32)
 	point = models.IntField()

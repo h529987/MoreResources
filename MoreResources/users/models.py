a718537b84676c4cfaa64510415fbdbb0e5b9201
leonyan <leonyan.loveoflife@gmail.com>
2018-05-29 14:50:08 +0800

    correct IntegerFiled in the normal_user table

--- MoreResources/users/models.py
+++ MoreResources/users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class normal_user(models.Model):
-	user_id = models.IntField(primary_key = True)
+	user_id = models.IntegerField(primary_key = True)
 	user_name = models.CharField(max_length = 64)
 	user_passwd = models.CharField(max_length = 32)
-	point = models.IntField(null = True)
+	point = models.IntegerField(null = True)
 	type = models.CharField(max_length = 16)
-	image = models.BlobField(null = True)
+	image = models.ImageField(null = True)
 	introduction = models.CharField(max_length = 100, null = True)

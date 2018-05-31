2480b2f550bd590bbeb542c02a1efaa267e53cbd
joseph <joseph.vuoto@gmail.com>
2018-05-31 19:17:35 +0800

    registered the models and created a superuser

--- MoreResources/users/admin.py
+++ MoreResources/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import normal_user, expert, administrator
+
+admin.site.register(normal_user)
+admin.site.register(expert)
+admin.site.register(administrator)

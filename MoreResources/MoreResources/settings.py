011feb124ec62ac58a3027f05640685c355fe858
leonyan <leonyan5@sina.cn>
2018-06-01 01:21:57 +0800

    change mysql passwd

--- MoreResources/MoreResources/settings.py
+++ MoreResources/MoreResources/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'MoreResources',
         'USER': "root",
-        'PASSWORD': 'passwd',
+        'PASSWORD': '123456789',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }

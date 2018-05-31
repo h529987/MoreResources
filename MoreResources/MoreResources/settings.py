8f2a025878ff1cd7836f3b60aa858bf71c90526b
joseph <joseph.vuoto@gmail.com>
2018-05-31 13:50:55 +0800

    simple api demo created by Django REST framework

--- MoreResources/MoreResources/settings.py
+++ MoreResources/MoreResources/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'MoreResources',
         'USER': "root",
-        'PASSWORD': '1234five',
+        'PASSWORD': 'passwd',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
